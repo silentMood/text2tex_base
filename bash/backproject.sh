4a3ead153afbd1f27274dbbabc2cc6a0b82e4117
Herbert <herbertdaicoding@gmail.com>
2025-03-19 20:22:40 -0400

    add backproject part

---
+++ bash/backproject.sh
@@ -0,0 +1,22 @@
+python scripts/backprject_albedo_mat.py \
+    --input_dir data/scene/ \
+    --output_dir outputs/scene \
+    --obj_name mesh \
+    --obj_file mesh.obj \
+    --prompt "a japanese style living room" \
+    --add_view_to_prompt \
+    --ddim_steps 50 \
+    --new_strength 1 \
+    --update_strength 0.3 \
+    --view_threshold 0.1 \
+    --blend 0 \
+    --dist 0.1 \
+    --num_viewpoints 36 \
+    --viewpoint_mode predefined \
+    --use_principle \
+    --update_steps 20 \
+    --update_mode heuristic \
+    --seed 42 \
+    --post_process \
+    --device "2080" \
+    --use_objaverse # assume the mesh is normalized with y-axis as up
